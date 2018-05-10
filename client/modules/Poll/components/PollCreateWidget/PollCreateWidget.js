30bd35e11486df0138b36ee34d10b7464debd17b
Nick Eliason <nceliason@gmail.com>
2018-05-09 22:40:19 -0700

    allow more than 2 choices when creating poll

--- client/modules/Poll/components/PollCreateWidget/PollCreateWidget.js
+++ client/modules/Poll/components/PollCreateWidget/PollCreateWidget.js
@@ -6,14 +6,67 @@ import { Button, ButtonToolbar } from 'react-bootstrap';
 import styles from './PollCreateWidget.css';
 
 export class PollCreateWidget extends Component {
+
+  state = {
+    numOptions: 2,
+    options: [
+      {
+        text: '',
+        placeholder: 'Blue',
+      },
+      {
+        text: '',
+        placeholder: 'Red',
+      },
+    ],
+  };
+
+  onOptionChange = (event, index) => {
+    const optionText = event.target.value;
+    const newOptions = this.state.options.map((option, mapIndex) => {
+      if (mapIndex === index) {
+        return {
+          ...option,
+          text: optionText,
+        };
+      }
+      return option;
+    });
+    this.setState({ options: newOptions });
+  }
+
+  addOption = () => {
+    const newOptions = [
+      ...this.state.options,
+      {
+        text: '',
+        placeholder: `choice ${this.state.options.length + 1}`,
+      },
+    ];
+    this.setState({
+      options: newOptions,
+    });
+  }
+
   addPoll = () => {
     const nameRef = this.refs.name;
     const titleRef = this.refs.title;
-    const choiceRef1 = this.refs.choice1;
-    const choiceRef2 = this.refs.choice2;
-    if (nameRef.value && titleRef.value && this.refs.choice1 && this.refs.choice2) {
-      this.props.addPoll(nameRef.value, titleRef.value, [choiceRef1.value, choiceRef2.value]);
-      nameRef.value = titleRef.value = choiceRef1.value = choiceRef2.value = '';
+    const choices = this.state.options.map((option) => {
+      return option.text;
+    });
+    console.log('choices:', choices);
+    if (nameRef.value && titleRef.value && choices.length >= 2) {
+      this.props.addPoll(nameRef.value, titleRef.value, choices);
+      nameRef.value = titleRef.value = '';
+      const newOptions = this.state.options
+        .filter((option, index) => index < 2)
+        .map((option) => {
+          return {
+            ...option,
+            text: '',
+          };
+        });
+      this.setState({ options: newOptions });
     }
   };
 
@@ -26,11 +79,18 @@ export class PollCreateWidget extends Component {
           <input placeholder="Author's Name" className={styles['form-field']} ref="name" />
           <input placeholder="What is your favorite color?" className={styles['form-field']} ref="title" />
           Choices
-          <input placeholder="Blue" className={styles['form-field']} ref="choice1" />
-          <input placeholder="Red" className={styles['form-field']} ref="choice2" />
+          {this.state.options.map((option, index) =>
+            <input
+              placeholder={option.placeholder}
+              className={styles['form-field']}
+              onChange={(event) => this.onOptionChange(event, index)}
+              value={this.state.options[index].text}
+              key={index}
+            />
+          )}
           <ButtonToolbar>
             <Button bsStyle="primary" href="#" onClick={this.addPoll}>Submit</Button>
-            <Button onClick={this.addChoice}>Add Choice</Button>
+            <Button onClick={this.addOption}>Add Choice</Button>
           </ButtonToolbar>
         </div>
       </div>
